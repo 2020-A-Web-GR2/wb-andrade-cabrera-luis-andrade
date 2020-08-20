27121d0f153295fbfd4adab48007dc16342252b4
andr3slelouch <luis.andradec14@gmail.com>
2020-08-20 16:12:08 -0500

    Clase 22 - Utilizar servicios en diferentes módulos y vistas.

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const cookieParser = require('cookie-parser');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieParser = require('cookie-parser'); //Importar cosas con JS
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express'); //Importar cosas con JS
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as any;
   /*Aquí irá cualquier configuración antes del app.listen
   luego de esto no se ejecutará ningun código*/
   app.use(cookieParser('Me agradan los poliperros'))
+  app.set('view engine','ejs')
+  app.use(express.static('publico'));
   await app.listen(3001);
 }
 bootstrap();
